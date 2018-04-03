c1a050e3174934c1afd5df31d3ffc57f4bae0b1f
胡俊杰 <hujj@crop.hxqc.com>
2018-04-03 10:49:51 +0800

    init

---
+++ openCVSamplefacedetection/src/main/java/org/opencv/samples/facedetect/a.java
@@ -0,0 +1,42 @@
+package org.opencv.samples.facedetect;
+
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created 胡俊杰
+ * 2018/3/28.
+ * Todo:
+ */
+
+public class a {
+
+
+	/**
+	 * 保存图片
+	 *
+	 * @param strPath
+	 * @param patamBitmap
+	 * @return
+	 */
+	public static boolean savaQualityBitmap(String strPath, Bitmap patamBitmap, int quality) {
+
+//		makeFileDirs(strPath);
+		File localFile = new File(strPath);
+		try {
+			FileOutputStream localFileOutputStream = new FileOutputStream(
+					localFile);
+			patamBitmap.compress(Bitmap.CompressFormat.JPEG, quality,
+					localFileOutputStream);
+			localFileOutputStream.flush();
+			localFileOutputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+}
